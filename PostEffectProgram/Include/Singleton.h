1ac1940712d6f9cfcf401c418de09061ec969a4e
germain.mazac <germain.mazac@7ea0d518-2368-11de-96a1-5542d1fc8aa6>
2009-06-20 18:19:21 +0000

    Ajout de Singleton.h

---
+++ PostEffectProgram/Include/Singleton.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <stdlib.h>
+
+template< typename T >
+class Singleton
+{
+public:
+
+
+	static T* GetSingleton( void )
+	{
+		if( !m_Instance )
+			m_Instance = new T;
+
+		return m_Instance;
+	}
+
+	static void Destroy( void )
+	{
+		if( m_Instance )
+		{
+			delete m_Instance;
+			m_Instance = NULL;
+		}
+	}
+
+
+protected:
+
+	Singleton			( void ){}		
+	virtual ~Singleton	( void ){}	
+
+
+private:
+
+	static T	*m_Instance;		
+
+};
+
+template< typename T >
+T* Singleton< T >::m_Instance = NULL;
+
