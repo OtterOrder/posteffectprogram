9add90d00406f0e9cad68693cef6c49186a2cda8
germain.mazac <germain.mazac@7ea0d518-2368-11de-96a1-5542d1fc8aa6>
2009-06-20 19:35:08 +0000

    Maj du GBufferRenderer

--- trunk/PostEffectProgram/Src/GBufferRenderer.cpp
+++ trunk/PostEffectProgram/Src/GBufferRenderer.cpp
@@ -1,16 +1,24 @@
 
 #include "GBufferRenderer.h"
 #include "Scene.h"
+#include "GraphicEntity.h"
+
 
 
 GBufferRenderer::GBufferRenderer()
 {
-
+	m_Camera.SetProjParams(D3DX_PI/4, (float)800/600, 0.1f, 100.f);
 }
 
 GBufferRenderer::~GBufferRenderer()
 {
 
+}
+
+void GBufferRenderer::SetScene(Scene *_scene)
+{
+	m_pScene=_scene;
+	m_pEntityList=m_pScene->GetGraphicEntityList();
 }
 
 
@@ -22,20 +30,39 @@ void GBufferRenderer::RenderScene()
 
 void GBufferRenderer::ComputeMatrices()
 {
+	GraphicEntity::Iterator GEntityIt;
+	D3DXMATRIX viewProj, WorldViewProj;
+	D3DXMatrixMultiply(&viewProj, m_Camera.GetViewMatrix(), m_Camera.GetProjMatrix());
 
+	for(GEntityIt=m_pEntityList->begin();  GEntityIt != m_pEntityList->end(); GEntityIt++)
+	{	
+		D3DXMatrixMultiply(&WorldViewProj, &(*GEntityIt)->GetWorldMatrix(), &viewProj);
+	}
+	
 }
 
 void GBufferRenderer::RenderGBufferPass()
 {
 
+	// Assignation des render targer
+	// Assignation du shader
 	Render();
 
 }
 
-
-
-
 void GBufferRenderer::Render()
 {
+
+	m_Camera.FrameMove(m_Timer.GetDeltaTimeF());
+
+    m_pDevice->Clear( 0, NULL, D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER,D3DCOLOR_XRGB(0,0,125), 1.0f, 0 );
+
+	GraphicEntity::Iterator GEntityIt;
+	for(GEntityIt=m_pEntityList->begin();  GEntityIt != m_pEntityList->end(); GEntityIt++)
+	{
+	//	(*GEntityIt)->Draw();
+	}
+
+	m_Timer.EndF();
 
 }
