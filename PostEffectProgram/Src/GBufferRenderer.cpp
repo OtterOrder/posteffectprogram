e2c1364e8b0dfd45d0dfacf750d15214f270657c
germain.mazac <germain.mazac@7ea0d518-2368-11de-96a1-5542d1fc8aa6>
2009-06-26 09:55:32 +0000

    Ajout de la nouvelle scene. Sortie du GBuffer finalisée.

--- PostEffectProgram/Src/GBufferRenderer.cpp
+++ PostEffectProgram/Src/GBufferRenderer.cpp
@@ -7,7 +7,8 @@
 
 GBufferRenderer::GBufferRenderer()
 {
-	
+	m_fZNear=0.5f;
+	m_fZFar=300.f;
 }
 
 GBufferRenderer::~GBufferRenderer()
@@ -21,14 +22,14 @@ void GBufferRenderer::SetScene(Scene *_scene)
 	m_pEntityList=m_pScene->GetGraphicEntityList();
 }
 
-void GBufferRenderer::Init(PDevice _Device, int _width, int _height)
+void GBufferRenderer::Init(PDevice _Device, u32 _width, u32 _height)
 {
 	m_pDevice=_Device;
-	m_iBackBufferWidth=_width;
-	m_iBackBufferHeight=_height;
+	m_BackBufferWidth=_width;
+	m_BackBufferHeight=_height;
 
-	m_Camera.SetProjParams(D3DX_PI/4, (float)_width/_height, 0.1f, 100.f);
-	m_Camera.SetViewParams(&D3DXVECTOR3(0.f, 0.f, -50.f), &D3DXVECTOR3(0.f, 0.f, 0.f));
+	m_Camera.SetProjParams(D3DX_PI/4, (float)_width/_height, m_fZNear, m_fZFar);
+	m_Camera.SetViewParams(&D3DXVECTOR3(-100.f, 100.f, -100.f), &D3DXVECTOR3(0.f, 0.f, 0.f));
 
 	// Initialisation shader
 	m_GRendererMaterial.SetShader(m_pDevice, "..\\Datas\\Shaders\\VSGbuffer.vsh", "VSTest", "..\\Datas\\Shaders\\PSGbuffer.psh", "PSTextureDisplay");
@@ -36,7 +37,7 @@ void GBufferRenderer::Init(PDevice _Device, int _width, int _height)
 	// Initialisation Render target
 	m_GBuffer.m_pRTSceneDiffuseMap.Create(m_pDevice, Vector2i(_width, _height));
 	m_GBuffer.m_pRTSceneNormalMap.Create(m_pDevice, Vector2i(_width, _height));
-	m_GBuffer.m_pRTDepthMap.Create(m_pDevice, Vector2i(_width, _height), D3DFMT_R32F);
+	m_GBuffer.m_pRTDepthMap.Create(m_pDevice, Vector2i(_width, _height));
 	m_pDevice->CreateDepthStencilSurface( _width, _height, D3DFMT_D16, D3DMULTISAMPLE_NONE, 0, TRUE, &m_pShadowDepth, NULL );
 
 }
@@ -69,6 +70,8 @@ void GBufferRenderer::ComputeMatrices(Matrix _world)
 	MatrixMultiply(&WorldViewProj, &_world, &viewProj);
 	m_GRendererMaterial.m_pShader->SetVSMatrix(m_pDevice, "g_mWorld", _world);
 	m_GRendererMaterial.m_pShader->SetVSMatrix(m_pDevice, "g_mWorldViewProjection", WorldViewProj);
+	m_GRendererMaterial.m_pShader->SetPSFloat(m_pDevice, "g_fZNear", m_fZNear);
+	m_GRendererMaterial.m_pShader->SetPSFloat(m_pDevice, "g_fZFar", m_fZFar);
 	
 }
 
@@ -85,6 +88,10 @@ void GBufferRenderer::RenderGBufferPass()
 
 	m_pDevice->Clear( 0, NULL, D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0,0,125), 1.0f, 0 );
 	Render();
+
+	D3DXSaveTextureToFile("testdiff.bmp", D3DXIFF_BMP, m_GBuffer.m_pRTSceneDiffuseMap.GetTexture(), NULL);
+	D3DXSaveTextureToFile("testnormal.bmp", D3DXIFF_BMP, m_GBuffer.m_pRTSceneNormalMap.GetTexture(), NULL);
+	D3DXSaveTextureToFile("testz.bmp", D3DXIFF_BMP, m_GBuffer.m_pRTDepthMap.GetTexture(), NULL);
 
 	// Désactivation render target
 	PSurface pd3dBackBufferSurface;
